64c6583984d7aba412151bc47efe0277a8ce76d3
João Aragão <emptechno@gmail.com>
2020-03-03 20:20:55 +0000

    -- removed arquillian fix attempt #2

--- singletonpattern/src/test/java/com/es2/singleton/EndTest.java
+++ singletonpattern/src/test/java/com/es2/singleton/EndTest.java
@@ -1,19 +1,8 @@
 package com.es2.singleton;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.runner.RunWith;
 
-@RunWith(Arquillian.class)
 public class EndTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(End.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+
 
 }
