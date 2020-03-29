c111ad62d959b3193ff6759d4a712eb3ada4cdce
João Aragão <emptechno@gmail.com>
2020-03-29 20:27:02 +0100

    -- travis test junit5 switch (farmar commits lol.... build error fix #3)

--- singletonpattern/src/main/java/com/es2/singleton/Singleton.java
+++ singletonpattern/src/main/java/com/es2/singleton/Singleton.java
@@ -21,8 +21,6 @@ public class Singleton {
     }
 
     public static void main(String[] args) {
-        Registry registry = Registry.Registry("1", "1");
-        Registry registry2 = Registry.Registry("2", "2");
-        printPath(registry, registry2);
+
     }
 }
